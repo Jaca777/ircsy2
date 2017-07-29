43f708e24fe4b040c53e65f197f739c4765a4fa4
Szymon Rodziewicz <szymon.rodziewicz@intern.allegrogroup.com>
2017-07-29 12:37:59 +0200

    Initial commit

---
+++ src/main/java/pl/jaca/ircsy/IrcsyApplication.java
@@ -0,0 +1,12 @@
+package pl.jaca.ircsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrcsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrcsyApplication.class, args);
+	}
+}
